6fd92e2430cafdf03e313683358b0ced8fe715d3
TheAllFather000 <ibrahimsow367@gmail.com>
2026-04-20 22:20:25 +0200

    Update all API URLs to ngrok tunnel

--- linkhive-frontend/config.js
+++ linkhive-frontend/config.js
@@ -1,2 +1,4 @@
 window.GRANUM_API_URL = 'https://unhid-untriflingly-georgiann.ngrok-free.dev';
-localStorage.setItem('granum_api_url', 'https://unhid-untriflingly-georgiann.ngrok-free.dev');
+if (!localStorage.getItem('granum_api_url')) {
+  localStorage.setItem('granum_api_url', 'https://unhid-untriflingly-georgiann.ngrok-free.dev');
+}
